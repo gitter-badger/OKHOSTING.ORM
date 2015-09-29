9ca74d95081ad80d320795ecd98a2b7bc378c2b4
David Muñoz <david@okhosting.com>
2015-09-28 19:10:22 -0500

    Added web form templates

---
+++ src/OKHOSTING.Authentication/DbUserPermission.cs
@@ -0,0 +1,19 @@
+﻿using OKHOSTING.Data.Validation;
+
+namespace OKHOSTING.Authentication
+{
+	/// <summary>
+	/// Represents a DataType and a list of DataMembers that a user has acces to
+	/// </summary>
+	public class DbUserPermission : DbPermission
+	{
+		/// <summary>
+		/// User which this permission is assigned to
+		/// </summary>
+		[RequiredValidator]
+		public User User
+		{
+			get; set;
+		}
+	}
+}
